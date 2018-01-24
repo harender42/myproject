b08e9b3a7b98463e72ba32e9842a301c47df7472
harender42 <harender42.kapkoti@gmail.com>
2018-01-24 11:32:24 -1200

    File added

---
+++ string.py
@@ -0,0 +1,10 @@
+'spam eggs'
+'does \ t'
+'"yes,"he said.'
+'"Isn\'t,"she said.'
+s='First line.\n Second line.'
+s
+print(s)
+print('c:\some\name')
+print(r'c:\some\name')
+text=('put something to add''to have them joined together')
